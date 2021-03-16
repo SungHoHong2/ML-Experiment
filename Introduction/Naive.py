eb799cd243f89a0bbafbdd9ffef82c34ef35d841
maverickjin88 <maverickjin88@gmail.com>
2021-03-15 17:59:01 -0700

    update

--- Introduction/Naive.py
+++ Introduction/Naive.py
@@ -1,7 +1,13 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 
+# samples
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+# labels
 Y = np.array([1, 1, 1, 2, 2, 2])
-
-print('working')
+# call the classifier
+clf = GaussianNB()
+# fit the samples and the labels to the classifier
+clf.fit(X, Y)
+# predict
+print(clf.predict([[-0.8, -1]]))
