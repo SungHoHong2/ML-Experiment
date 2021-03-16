c9b32c0d8952ceefceea8051da9d559a7ef48f2b
maverickjin88 <maverickjin88@gmail.com>
2021-03-15 17:48:58 -0700

    update

---
+++ version.sh
@@ -0,0 +1,14 @@
+git config credential.helper store
+git pull origin master
+LOCATION="$(pwd)"
+VERSION=3
+
+if [ "$1" = "commit" ]
+then
+  git add .
+  git commit -m "$2"
+  git push origin master
+
+else
+  echo "no argument"
+fi
