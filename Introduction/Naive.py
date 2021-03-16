9730d29e004f383573fe6cb1142a8bd1c347dc6d
maverickjin88 <maverickjin88@gmail.com>
2021-03-15 17:53:37 -0700

    update

--- Introduction/Naive.py
+++ Introduction/Naive.py
@@ -1,1 +1,7 @@
-print('hello!')
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+
+print('working')
