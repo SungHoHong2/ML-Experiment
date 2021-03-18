daabb8dea66b5bddae6c679f4bf172c810f08931
maverickjin88 <maverickjin88@gmail.com>
2021-03-18 13:37:26 -0700

    DT

--- Movie/App.py
+++ Movie/App.py
@@ -29,7 +29,7 @@ data_frame = pd.read_csv(PATH+'/total_data.csv', sep=',', names=['movieId', 'use
 data_frame.date = pd.to_datetime(data_frame.date)
 data_frame.sort_values(by='date', inplace=True)
 
-for column_name, item in data_frame.iteritems()[:10]:
+for column_name, item in data_frame.head(10).iteritems():
     print(column_name, item)
 
 
