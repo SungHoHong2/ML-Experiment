72310787d4ea13534787429aa00149e5ce513c23
maverickjin88 <maverickjin88@gmail.com>
2021-03-18 14:50:06 -0700

    Movie

--- Movie/App.py
+++ Movie/App.py
@@ -133,6 +133,5 @@ print("\nUser has {} Ratings.".format(spareMatrixTrain[usr_id,:].getnnz()))
 print("\nWe have {} users which are similar to this user".format(u_u_sim_sparse[mv_id,:].getnnz()))
 
 
-
-
-
+for tmp in u_u_sim_sparse:
+    print(tmp)
