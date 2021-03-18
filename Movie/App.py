fcb5a6f438ec8b7b13db05dc51392c6bdd2d5f8a
maverickjin88 <maverickjin88@gmail.com>
2021-03-18 13:55:08 -0700

    DT

--- Movie/App.py
+++ Movie/App.py
@@ -30,8 +30,8 @@ data_frame.date = pd.to_datetime(data_frame.date)
 data_frame.sort_values(by='date', inplace=True)
 
 # print out the top 10 rows of the data
-for row in data_frame.head(10).itertuples():
-    print(row)
+# for row in data_frame.head(10).itertuples():
+#     print(row)
 
 # split the data into training and test data
 data_frame.iloc[:int(data_frame.shape[0]*0.80)].to_csv(PATH+"train.csv", index=False)
@@ -60,4 +60,54 @@ us,mv = sparseMatrixTest.shape
 elem = sparseMatrixTest.count_nonzero()
 print("Sparsity of Testing matrix : {} % ".format(  (1-(elem/(us*mv))) * 100) )
 
+# create the similarity matrix
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def compute_user_similarity(sparse_matrix, compute_for_few=False, top=100, verbose=False, verb_for_n_rows=20):
+    no_of_users, _ = sparse_matrix.shape
+    row_ind, col_ind = sparse_matrix.nonzero()
+    row_ind = sorted(set(row_ind))
+    time_taken = list()
+
+    rows, cols, data = list(), list(), list()
+    if verbose: print("Computing top", top, "similarities for each user..")
+
+    start = datetime.now()
+    temp = 0
+
+    for row in row_ind[:top] if compute_for_few else row_ind:
+        temp = temp + 1
+        prev = datetime.now()
+
+        sim = cosine_similarity(sparse_matrix.getrow(row), sparse_matrix).ravel()
+        top_sim_ind = sim.argsort()[-top:]
+        top_sim_val = sim[top_sim_ind]
+
+        rows.extend([row] * top)
+        cols.extend(top_sim_ind)
+        data.extend(top_sim_val)
+        time_taken.append(datetime.now().timestamp() - prev.timestamp())
+        if verbose:
+            if temp % verb_for_n_rows == 0:
+                print("computing done for {} users [  time elapsed : {}  ]"
+                      .format(temp, datetime.now() - start))
+
+    if verbose: print('Creating Sparse matrix from the computed similarities')
+
+    # if draw_time_taken:
+    #     plt.plot(time_taken, label='time taken for each user')
+    #     plt.plot(np.cumsum(time_taken), label='Total time')
+    #     plt.legend(loc='best')
+    #     plt.xlabel('User')
+    #     plt.ylabel('Time (seconds)')
+    #     plt.show()
+
+    return sparse.csr_matrix((data, (rows, cols)), shape=(no_of_users, no_of_users)), time_taken
+
+u_u_sim_sparse, _ = compute_user_similarity(spareMatrixTrain, compute_for_few=True, top = 100, verbose=True)
+
+
+
+# Predict using similarity matrix
 
