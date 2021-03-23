00d9a3c68d8d5714ce3b763937c9d7cfc4619905
maverickjin88 <maverickjin88@gmail.com>
2021-03-23 15:02:14 -0700

    Pytorch Tutorial

--- ML_Pytorch_Intro/pytorch_tutorial.py
+++ ML_Pytorch_Intro/pytorch_tutorial.py
@@ -7,4 +7,23 @@ torch.rand(2,3)
 x = torch.zeros(2,3)
 # check the type of the matrix
 print(x.dtype)
+# set up 2d zero matrix with int values
+x = torch.zeros(2,3, dtype=torch.int)
+x = torch.zeros(2,3, dtype=torch.float16)
+x = torch.zeros(2,3, dtype=torch.double)
+print(x.dtype)
+print(x.size())
+x = torch.tensor([2.5,0.1])
+
+# adding the tensor
+x = torch.rand(2,2)
+y = torch.rand(2,2)
+print(x)
+print(y)
+
+
+
+
+
+
 
