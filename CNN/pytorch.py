1c57f3852ec1f079bc7d3472e3eb24c86f1e4270
maverickjin88 <maverickjin88@gmail.com>
2021-03-24 11:27:07 -0700

    Pytorch Tutorial

--- CNN/pytorch.py
+++ CNN/pytorch.py
@@ -1,34 +1,10 @@
 import torch
-# create a 3d tensor
-torch.empty(2,2,3)
-# create 2d random tensor
-torch.rand(2,3)
-# create 2d zero tensor
-x = torch.zeros(2,3)
-# check the type of the tensor
-# print(x.dtype)
-# set up 2d zero tensor with int values
-x = torch.zeros(2,3, dtype=torch.int)
-x = torch.zeros(2,3, dtype=torch.float16)
-x = torch.zeros(2,3, dtype=torch.double)
-# print(x.dtype)
-# print(x.size())
-# create a tensor directly
-x = torch.tensor([2.5,0.1])
-# adding the tensor
-x = torch.rand(2,2)
-y = torch.rand(2,2)
-# print(x)
-# print(y)
-z = x + y
-z = torch.add(x,y)
-z = y.add_(x)
-# print(z)
-# print out all the rows of column 0
-x = torch.rand(5,3)
-print(x)
+t = Tensor([[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]])
 
-print(x[:, 0])
+print(t.shape)
+
 
 
 
