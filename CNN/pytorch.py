6e43f4125bb24e2d73d620dedec80b3ca6802e7a
maverickjin88 <maverickjin88@gmail.com>
2021-03-24 11:39:30 -0700

    Pytorch Tutorial

--- CNN/pytorch.py
+++ CNN/pytorch.py
@@ -1,11 +1,13 @@
 import torch
 t = torch.Tensor([[1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]])
+                  [4, 5, 6],
+                  [7, 8, 9]])
 
 print(t.shape)
 
-
+t = torch.Tensor([[1, 2, 3],
+                  [4, 5, 6]])
+print(t.shape)
 
 
 
