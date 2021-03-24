1e695dbab62dfda7d3532e79259c26650e3d7343
maverickjin88 <maverickjin88@gmail.com>
2021-03-24 11:42:10 -0700

    Pytorch Tutorial

--- CNN/pytorch.py
+++ CNN/pytorch.py
@@ -3,12 +3,11 @@ t = torch.Tensor([[1, 2, 3],
                   [4, 5, 6],
                   [7, 8, 9]])
 
+# shape of 3 rows and 3 columns
 print(t.shape)
 
-t = torch.Tensor([[1, 2, 3],
-                  [4, 5, 6]])
-print(t.shape)
-
+# shape of 1 row with 9 columns
+print(t.reshape(1,9))
 
 
 
