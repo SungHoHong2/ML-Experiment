ac36fbc7fd6eb9f92e9d8a4fbf64857c4b0d963b
maverickjin88 <maverickjin88@gmail.com>
2021-03-24 15:49:36 -0700

    Pytorch Tutorial

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -18,9 +18,24 @@ train_set = torchvision.datasets.FashionMNIST(
 # allows to shuffle, and provie batch-size of the data
 train_loader = torch.utils.data.DataLoader(train_set, batch_size = 10)
 
-print(len(train_set))
-print(train_set.train_labels)
-print(train_set.train_labels.bincount())
+"""
+reading the training set 
+"""
+# print(len(train_set))
+# print(train_set.train_labels)
+# give use the frequency distribution of the labels
+# print(train_set.train_labels.bincount())
+
+"""
+reading a training sample 
+"""
+sample = next(iter(train_set))
+print(len(sample))
+image, label = sample
+
+
+
+
 
 
 
