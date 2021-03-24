94b359247df9931beb2493b15bf27110d790efa4
maverickjin88 <maverickjin88@gmail.com>
2021-03-24 12:10:49 -0700

    Pytorch Tutorial

--- CNN/pytorch.py
+++ CNN/pytorch.py
@@ -4,19 +4,23 @@ t = torch.Tensor([[1, 2, 3],
                   [7, 8, 9]])
 
 # shape of 3 rows and 3 columns
-print(t.shape)
+# print(t.shape)
 
 # shape of 1 row with 9 columns
-print(t.reshape(1,9))
-
+# print(t.reshape(1,9))
 
 # Create tensors without data
-print(torch.eye(2))
-print(torch.zeros(2,2))
-print(torch.ones(2,2))
-print(torch.rand(2,2))
+# print(torch.eye(2))
+# print(torch.zeros(2,2))
+# print(torch.ones(2,2))
+# print(torch.rand(2,2))
 
-
+# flatten function
+def flatten(t):
+    t = t.reshape(1,-1)
+    t = t.squeeze()
+    return t
+print(squeeze(t))
 
 
 
