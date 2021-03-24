a2407c894e4d5ff8cb74c5884b07d1813c4d3d43
maverickjin88 <maverickjin88@gmail.com>
2021-03-23 17:57:43 -0700

    Pytorch Tutorial

--- CNN/linear.py
+++ CNN/linear.py
@@ -1,5 +1,23 @@
-print('howdy')
+
+def plus(array1, array2):
+    res = list()
+    for i in range(len(array1)):
+        res.append(array1[i] + array2[i])
+    return res
+
+def minus(array1, array2):
+    res = list()
+    for i in range(len(array1)):
+        res.append(array1[i] - array2[i])
+    return res
+
+def multiply(num, array1):
+    res = list()
+    for i in range(len(array1)):
+        res.append(array1[i] * num)
+    return res
 
 
 
+print(plus([8.218,-9.341], [-1.129,2.111]))
 
