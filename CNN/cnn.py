a766949d3ae667f02276681dddcfacacdc4f94d4
maverickjin88 <maverickjin88@gmail.com>
2021-03-25 16:26:53 -0700

    update

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,10 +1,52 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+import torch.optim as optim
+
 torch.set_printoptions(linewidth=120)
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+        self.fc1 = nn.Linear(in_features=12 * 4 * 4, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=60)
+        self.out = nn.Linear(in_features=60, out_features=10)
+
+    def forward(self, t):
+        # (1) input layer
+        t = t
+
+        # (2) hidden conv layer
+        t = self.conv1(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+        # (3) hidden conv layer
+        t = self.conv2(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)
+
+        # (4) hidden linear layer
+        t = t.reshape(-1, 12 * 4 * 4)
+        t = self.fc1(t)
+        t = F.relu(t)
+
+        # (5) hidden linear layer
+        t = self.fc2(t)
+        t = F.relu(t)
+
+        # (6) ouptut layer
+        t = self.out(t)
+        # output: [1,10]
+        # t = F.softmax(t, dim=1)
+        return t
 
 train_set = torchvision.datasets.FashionMNIST(
     root = '/Users/sunghohong/Desktop/data/FashionMNIST',
@@ -15,40 +57,47 @@ train_set = torchvision.datasets.FashionMNIST(
     ])
 )
 
-# allows to shuffle, and provie batch-size of the data
-train_loader = torch.utils.data.DataLoader(train_set, batch_size = 10)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size = 100, shuffle=True)
+network = Network()
+optimizer = optim.Adam(network.parameters(), lr=0.01)
 
-"""
-reading the training set 
-"""
-# print(len(train_set))
-# print(train_set.train_labels)
-# give use the frequency distribution of the labels
-# print(train_set.train_labels.bincount())
+num_of_pixels = len(train_set) * 28 * 28
+total_sum = 0
+for batch in train_loader:
+    total_sum += batch[0].sum()
 
-"""
-reading a training sample 
-"""
-sample = next(iter(train_set))
-# sample = train_set[0]
-print(len(sample))
-# return a 2D array with a label
-image, label = sample
-print(image, label)
+mean = total_sum / num_of_pixels
+sum_of_squared_error = 0
+for batch in train_loader:
+    sum_of_squared_error += ((batch[0]-mean).pow(2)).sum()
+std = torch.sqrt(sum_of_squared_error / num_of_pixels)
+print(mean, std)
 
-"""
-reading from a batch sample 
-"""
-batch = next(iter(train_loader))
-print(len(batch))
-images, labels = batch
+# images, labels = next(iter(train_loader))
+# grid = torchvision.utils.make_grid(images)
 
-print(len(images), len(labels))
-# 10 batches, 1 color, y=28, x=28
-print(images.shape)
-# 10 batches
-print(labels.shape)
+# tb = SummaryWriter()
+# tb.add_image('images', grid)
+# tb.add_graph(network, images)
 
-# http://localhost:8888/lab?token=33923aa70f01c416fcaaca14dde2cc9e0c0e6b7d5b43486c
-# http://192.168.0.15:8004/lab?token=33923aa70f01c416fcaaca14dde2cc9e0c0e6b7d5b43486c
-
+# for epoch in range(10):
+#     total_loss = 0
+#
+#     for batch in train_loader:
+#         images, labels = batch
+#
+#         preds = network(images)
+#         loss = F.cross_entropy(preds, labels)
+#
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#
+#         total_loss += loss.item()
+#
+#     print('epoch:', epoch, "loss", total_loss)
+#     tb.add_scalar('Loss', total_loss, epoch)
+#     tb.add_histogram('conv1.bais', network.conv1.bias, epoch)
+#     tb.add_histogram('conv1.weight', network.conv1.weight, epoch)
+#     tb.add_histogram('conv1.weight.grad', network.conv1.weight.grad, epoch)
+# tb.close()
